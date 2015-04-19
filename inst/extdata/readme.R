3190a0f9f62f8e479485893e20f2ec3b838931dc
Michael Sumner <mdsumner@gmail.com.com>
2015-04-19 01:31:17 +0000

    basics working now for polygons with holes

---
+++ inst/extdata/readme.R
@@ -0,0 +1,12 @@
+
+library(raster)
+library(rworldmap)
+data(countriesLow)
+## cut a hole in Australia
+##e <- drawPoly()
+library(rgeos)
+el <- SpatialPolygonsDataFrame(e, data.frame(x = 1))
+##b <- gDifference(countriesLow, el)
+brokeCountries  <- SpatialPolygonsDataFrame(gDifference(countriesLow, el, byid = TRUE), as.data.frame(countriesLow), match.ID = FALSE)
+
+save(brokeCountries, file = "inst/extdata/brokeCountries.Rdata")]
