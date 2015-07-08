c0714463398a09bb1c7e6320053470064cc8bb19
Michael Sumner <mdsumner@gmail.com.com>
2015-07-08 15:42:36 +1000

    clean up

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(gris)
+
+test_check("gris")
---
+++ tests/testthat/testhat_gris.R
@@ -0,0 +1,8 @@
+context("basic building")
+
+test_that("objects built from basic building blocks", {
+  ## from ?graphics::polygon
+  x <- c(1:9, 8:1)
+  y <- c(1, 2*(5:3), 2, -1, 17, 9, 8, 2:9)
+
+})
