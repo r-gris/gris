ba4930d079710256bb5d3d4725b63198fc516a67
Michael Sumner <mdsumner@gmail.com>
2016-01-10 10:39:38 +1100

    functions to add triangulated primitives to gris, internal for no

--- R/trianglePrimitives.R
+++ R/trianglePrimitives.R
@@ -1,49 +1,65 @@
-#  library(gris)
-#  library(maptools)
-#  data(wrld_simpl)
-#  gall <- gris(wrld_simpl)
-#  g <- gall[gall$o$NAME %in% c("Australia", "New Zealand"), ]
-#  
-#   library(RTriangle)
-#   tXv <-  oXt <- NULL
-#   maxtr <- 0
-#   
-#  # tXv - table with .vx1, .vx2, .vx3 and .tr0
-# #  oXt - table with .tr0 and .ob0
-# 
-#  oid <- unique(g$o$.ob0)
-#  for (i in seq_along(oid)) {
-#    id <- oid[i]
-#    ## we triangulate each branch individually, so we can keep track of them
-#    ##bXv <- g$bXv %>% filter(.br0 == id) %>% inner_join(g$v, ".vx0") %>% select(x, y, .vx0, .br0 )
-#    g0 <- g[g$o$.ob0 == id, ]
-#   ps <- mkpslg(g0)
-#    #ps <- pslg(P = bXv %>%  dplyr::select(x, y) %>% as.matrix(), S = gris:::prs1(seq(nrow(bXv))))
-#    tr <- RTriangle::triangulate(ps)
-#    #bXt <- data_frame(.br0 = rep(id, nrow(tr$T)), .tr0 = seq(nrow(tr$T)) + maxtr)
-#    oX <- data_frame(.ob0 = rep(id, nrow(tr$T)), .tr0 = seq(nrow(tr$T)) + maxtr)
-#    oXt <- bind_rows(oXt, oX)
-#    
-#    tX <- data_frame(.vx1 = g$v$.vx0[tr$T[,1]], .vx2 = g$v$.vx0[tr$T[,2]], .vx3 = g$v$.vx0[tr$T[,3]], 
-#                     .tr0 = oX$.tr0)
-#    
-#   tXv <- bind_rows(tXv, tX)
-#      maxtr <- maxtr + nrow(tr$T)
-# 
-# 
-#  }
-# 
-#  
-#  plotT <- function(tXv, v) {
-#    for (i in seq(nrow(tXv))) {
-#      XY1 <- v  %>% inner_join(tXv[i, ], c(".vx0" = ".vx1")) %>% select(x, y)
-#      XY2 <- v  %>% inner_join(tXv[i, ], c(".vx0" = ".vx2")) %>% select(x, y)
-#      XY3 <- v  %>% inner_join(tXv[i, ], c(".vx0" = ".vx3")) %>% select(x, y)
-#      
-#      
-#      polypath(c(XY1$x, XY2$x, XY3$x), c(XY1$y, XY2$y, XY3$y))
-#      
-#    }
-#    NULL
-#  }
-#  
+#' Add triangle primitives
+#'
+#' @param x gris object
+#'
+#' @examples
+#' \donttest{
+#' \dontrun{
+#' library(gris)
+#' library(maptools)
+#' data(wrld_simpl)
+#' gall <- gris(wrld_simpl)
+#' g <- gall[gall$o$NAME %in% c("Australia", "New Zealand"), ]
+#' g$v <- g$v %>% mutate(x = ifelse(x < 0, x + 360, x))
+#' library(RTriangle)
+#' gT <- triGris(g)
+#' plot(g)
+#' plotT(gT)
+#' }}
+triGris <- function(x) {
+  oid <- unique(x$o$.ob0)
+  
+  tXv <-  oXt <- NULL
+  maxtr <- 0
+  
+  for (i in seq_along(oid)) {
+    id <- oid[i]
+    ## we triangulate each branch individually, so we can keep track of them
+    ##bXv <- g$bXv %>% filter(.br0 == id) %>% inner_join(g$v, ".vx0") %>% select(x, y, .vx0, .br0 )
+    g0 <- x[x$o$.ob0 == id, ]
+    ps <- mkpslg(g0)
+    #ps <- pslg(P = bXv %>%  dplyr::select(x, y) %>% as.matrix(), S = gris:::prs1(seq(nrow(bXv))))
+    tr <- RTriangle::triangulate(ps)
+    #bXt <- data_frame(.br0 = rep(id, nrow(tr$T)), .tr0 = seq(nrow(tr$T)) + maxtr)
+    oX <- data_frame(.ob0 = rep(id, nrow(tr$T)), .tr0 = seq(nrow(tr$T)) + maxtr)
+    oXt <- bind_rows(oXt, oX)
+    
+    tX <- data_frame(.vx1 = g0$v$.vx0[tr$T[,1]], .vx2 = g0$v$.vx0[tr$T[,2]], .vx3 = g0$v$.vx0[tr$T[,3]],
+                     .tr0 = oX$.tr0)
+    
+    tXv <- bind_rows(tXv, tX)
+    maxtr <- maxtr + nrow(tr$T)
+    
+    
+  }
+  
+  x$tXv <- tXv
+  x$oXt <- oXt
+  x
+}
+
+plotT <- function(x) {
+  tXv <- x$tXv
+  v <- x$v
+  for (i in seq(nrow(tXv))) {
+    XY1 <- v  %>% inner_join(tXv[i, ], c(".vx0" = ".vx1")) %>% select(x, y)
+    XY2 <- v  %>% inner_join(tXv[i, ], c(".vx0" = ".vx2")) %>% select(x, y)
+    XY3 <- v  %>% inner_join(tXv[i, ], c(".vx0" = ".vx3")) %>% select(x, y)
+    
+    
+    polypath(c(XY1$x, XY2$x, XY3$x), c(XY1$y, XY2$y, XY3$y))
+    
+  }
+  NULL
+}
+
--- README.md
+++ README.md
@@ -13,16 +13,17 @@ What we have
  
 * basic S3 methods to build gris objects from sp and plot them. 
 * examples of various workflows
-* basic proj4::ptransform with dplyr piping
+* triangulated Primitives model (triGris and plotT internal functions)
+* coercion from gris to Spatial (need more on cycle-extraction for triangles)
+* on hold: ~basic proj4::ptransform with dplyr piping~
 
 What we need
 
-* integration of primitive model for topology
+* integration of primitive model for topology - this is working, but we don't record the branch each triangle belongs too - this is possible but could become meaningless if we start manipulating the mesh of triangles
 * need to fix how gris objects are created, since g[i, ] duplicates .ob0 for repeated i values - but maybe it's nonsense to allow repeats anyway?
 * flesh out the RTriangle support and relation to rgl texture mapping
 * ensure standard triangulation is done in a local projection, no matter what the source is in
 * add ability to explode and recombine objects and their component polygons arbitrarily
-* coercion from gris to Spatial*
 * apply system of naming for vertices - this is begun in gris.default, needs to be made only for simple case of points
 * ~convert raster to quad mesh~
 * ~~triangulation with CGAL, constrained Delaunay~~
