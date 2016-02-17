eccad0dbd22a60d010a876f967c483abbbf4d495
Michael Sumner <mdsumner@gmail.com>
2016-02-17 18:34:00 +1100

    Update gris.R

--- R/gris.R
+++ R/gris.R
@@ -19,8 +19,8 @@ mkpslg <- function(x) {
   ## remap vertices
   x$v$remap <- seq(nrow(x$v))
   x$bXv$.vx0 <- x$v$remap[match(x$bXv$.vx0, x$v$.vx0)]
-  pslg.default(
-    x = x$v %>% dplyr::select(x, y) %>% as.matrix(),
+  RTriangle::pslg(
+    P = x$v %>% dplyr::select(x, y) %>% as.matrix(),
     S = do.call(rbind, lapply(split(
       x$bXv$.vx0, x$bXv$.br0
     ), prs1))
