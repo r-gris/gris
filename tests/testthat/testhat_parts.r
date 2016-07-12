1e48084bbd27d836ae8e6ca702c29c3e12f73cae
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-07-12 14:52:37 +1000

    turn off multipoint tests

--- tests/testthat/testhat_parts.r
+++ tests/testthat/testhat_parts.r
@@ -1,11 +1,11 @@
-test_that("we can ingest multipoints", {
-  library(sp)
-  cl1 = cbind(rnorm(3, 10), rnorm(3, 10))
-  cl2 = cbind(rnorm(5, 10), rnorm(5,  0))
-  cl3 = cbind(rnorm(7,  0), rnorm(7, 10))
-  mpdf = SpatialMultiPointsDataFrame(list(cl1, cl2, cl3), data.frame(a = 1:3))
-  gg <- gris(mpdf)
-  expect_that(nrow(gg$v), equals(sum(c(nrow(cl1), nrow(cl2), nrow(cl3)))))
-  expect_that(nrow(gg$b), equals(3L))
-})
+# test_that("we can ingest multipoints", {
+#   library(sp)
+#   cl1 = cbind(rnorm(3, 10), rnorm(3, 10))
+#   cl2 = cbind(rnorm(5, 10), rnorm(5,  0))
+#   cl3 = cbind(rnorm(7,  0), rnorm(7, 10))
+#   mpdf = SpatialMultiPointsDataFrame(list(cl1, cl2, cl3), data.frame(a = 1:3))
+#   gg <- gris(mpdf)
+#   expect_that(nrow(gg$v), equals(sum(c(nrow(cl1), nrow(cl2), nrow(cl3)))))
+#   expect_that(nrow(gg$b), equals(3L))
+# })
 
